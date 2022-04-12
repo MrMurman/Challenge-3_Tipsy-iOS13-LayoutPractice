8c54e943c0eb26dca306c7ad5fd4373db0b45de7
MrMurman <74971676+MrMurman@users.noreply.github.com>
2022-04-12 11:48:05 +0300

    Initial Commit

---
+++ Challenge-3_Tipsy-iOS13-LayoutPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge-3_Tipsy-iOS13-LayoutPractice
+//
+//  Created by Андрей Бородкин on 12.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
